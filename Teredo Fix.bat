cc07413141537512298bf0cc73c90b193509919c
Dre Baldwin <39037261+baldw025@users.noreply.github.com>
2019-05-04 15:21:33 -0700

    Initial Upload

---
+++ Teredo Fix.bat
@@ -0,0 +1,5 @@
+REM Overwrites the Teredo Adapter "0x8e" state (Disabled) to "0x0" (Enabled). Forces "Yes" when asked to overwrite "DisabledComponents".
+reg add HKLM\System\CurrentControlSet\Services\Tcpip6\Parameters /f /v DisabledComponents /t REG_DWORD /d 0x0
+REM After executing the overwrite, simply restart the IP Helper service to eliminate the need to fully reboot the system to apply this fix.
+net stop "IP Helper" /y
+net start "IP Helper"
